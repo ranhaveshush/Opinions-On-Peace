f9d49eaaac4fcf12809051b8053e7dcb95fb2d26
Ran Haveshush <ran.haveshush.shenkar@gmail.com>
2013-06-02 20:01:36 +0300

    Initial commit

--- includes/js/visualize.js
+++ includes/js/visualize.js
@@ -1,4 +1,97 @@
-﻿var diameter = 760;
+﻿var margin = {top: 20, right: 100, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .05);
+
+var y = d3.scale.linear()
+    .domain([0, 100])
+    .rangeRound([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.json("includes/data/data.json", function(error, data) {
+
+    data.forEach(function(d) {
+        var children = d.children;
+        var cumulateHeight = 0;
+        children.forEach(function(d) {
+            d.y0 = cumulateHeight;
+            d.height = y(d.opinionValue);
+            cumulateHeight += d.height;
+        });
+    });
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  var month = svg.selectAll(".month")
+      .data(data)
+    .enter().append("g")
+      .attr("class", "month")
+      .attr("transform", function(d, i) { return "translate(" + x(i) + ",0)"; });
+
+  month.selectAll("rect")
+      .data(function(d) { return d.children; })
+    .enter().append("rect")
+      .attr("y", function(d) { return d.y0; })
+      .attr("width", x.rangeBand())
+      .attr("height", function(d) { return d.height; })
+      .style("fill", function(d) { return colors(d.opinionIndex); });
+/*
+  var legend = svg.select(".state:last-child").selectAll(".legend")
+      .data(function(d) { return d.ages; })
+    .enter().append("g")
+      .attr("class", "legend")
+      .attr("transform", function(d) { return "translate(" + x.rangeBand() / 2 + "," + y((d.y0 + d.y1) / 2) + ")"; });
+
+  legend.append("line")
+      .attr("x2", 10);
+
+  legend.append("text")
+      .attr("x", 13)
+      .attr("dy", ".35em")
+      .text(function(d) { return d.name; });
+*/
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var diameter = 760;
 var radius = diameter / 2;
 var range = d3.scale.linear()
     .domain([0, 100])
