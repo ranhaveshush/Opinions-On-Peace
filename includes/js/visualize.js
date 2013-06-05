747a2c8848967fb9468994988da208ff6bec44b8
Roni2012shemi <aharonshemi@gmail.com>
2013-06-05 23:42:34 +0300

    from ran

--- includes/js/visualize.js
+++ includes/js/visualize.js
@@ -1,10 +1,11 @@
-
 var margin = {top: 50, right: 40, bottom: 20, left: 40},
-    width = 1000 - margin.left - margin.right,
-    height = 700 - margin.top - margin.bottom;
+    width = 1200 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
+
+var numOfMonthsToDisplay = 8;
 
 var x = d3.scale.ordinal()
-	.rangeRoundBands([0, width], .5);
+    .rangeRoundBands([0, width], 0.5);
 
 var y = d3.scale.linear()
     .domain([0, 100])
@@ -14,42 +15,27 @@ var xAxis = d3.svg.axis()
     .scale(x)
     .orient("bottom");
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select("#wrapper").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-	/*d3.json("includes/data/data.json", function(error, data) {*/
 d3.json("includes/data/data.json", function(error, data) {
-	
-	var childrens;
-    data.forEach(function(d,i) {
-		switch(d.nationalitiesIndex){
-		case 0:	
-		childrens = d.children;
-		break;
-		case 1:	
-		childrens = d.children[1].children;
-		break;
-		case 2:	
-		childrens = d.children[2].children;
-		break;
-		}
-				
-        var cumulateHeight = 0;
-        childrens.forEach(function(d,i) {
-		    d.y0 = cumulateHeight;
-            d.height = y(d.opinionValue);
-            cumulateHeight += d.height;
-		 
-        })		
-		
-    });
 
-	x.domain(data.map(function(d,i) { 	
-		return i; 
-	}));
+  data = filterData(data);
+
+  data.forEach(function(d) {
+      var children = getChildren(d);
+      var cumulateHeight = 0;
+      children.forEach(function(d) {
+          d.y0 = cumulateHeight;
+          d.height = y(d.opinionValue);
+          cumulateHeight += d.height;
+      });
+  });
+
+  x.domain(data.map(function(d) { return d.month + "/" + d.year; }));
 
   svg.append("g")
       .attr("class", "x axis")
@@ -58,38 +44,18 @@ d3.json("includes/data/data.json", function(error, data) {
 
   var month = svg.selectAll(".month")
       .data(data)
-	  .enter()
-	.append("g")
-    .attr("class", "month")
-	.attr("transform", function(d,i) {
-	return "translate(" + x(i) + ",0)"; });
-		  
+    .enter().append("g")
+      .attr("class", "month")
+      .attr("transform", function(d, i) { var date = d.month + "/" + d.year; return "translate(" + x(date) + ",0)"; });
 
   month.selectAll("rect")
-      .data(function(d) { 
-	  var childrens;
-		
-	  switch(d.nationalitiesIndex){
-		case 0:	
-		childrens = d.children;
-		break;		
-		case 1:	
-		childrens = d.children[1].children;
-		break;
-		case 2:	
-		childrens = d.children[2].children;
-		break;
-		}
-	//console.log(childrens);		
-		return childrens;
-	   
-	  })
+      .data(function(d) { return getChildren(d); })
     .enter().append("rect")
       .attr("y", function(d) { return d.y0; })
       .attr("width", x.rangeBand())
       .attr("height", function(d) { return d.height; })
       .style("fill", function(d) { return colors[d.opinionIndex]; });
-/*
+
   var legend = svg.select(".state:last-child").selectAll(".legend")
       .data(function(d) { return d.ages; })
     .enter().append("g")
@@ -103,6 +69,14 @@ d3.json("includes/data/data.json", function(error, data) {
       .attr("x", 13)
       .attr("dy", ".35em")
       .text(function(d) { return d.name; });
-*/
 
 });
+
+function getChildren(datum) {
+    var nationalitiesIndex = datum.nationalitiesIndex;
+    return (nationalitiesIndex == 0) ? datum.children : datum.children[nationalitiesIndex].children;
+}
+
+function filterData(data) {
+    return data.slice(data.length - numOfMonthsToDisplay);
+}
