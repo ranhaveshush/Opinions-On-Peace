17a0c31b72088180f7cf1f555003960905db8255
ran <ran@Ran-vostro3450.(none)>
2013-06-09 23:54:23 +0300

    Both months and events scale to time, Removed ordinal scale

--- includes/js/visualize.js
+++ includes/js/visualize.js
@@ -2,14 +2,13 @@ var _margin = {top: 50, right: 40, bottom: 20, left: 40},
     width = 1200*4 - _margin.left - _margin.right,
     height = 600 - _margin.top - _margin.bottom;
 
-var _datesRange;
-
+// Check if this vars have to be global
 var _yTimeline = height;
 
-var x = d3.scale.ordinal()
-    .rangeRoundBands([0, width], 0.2);
+/*var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], 0.2);*/
 
-var xt = d3.time.scale()
+var x = d3.time.scale()
     .range([0, width]);
 
 var y = d3.scale.linear()
@@ -28,19 +27,22 @@ var svg = d3.select("#wrapper").append("svg")
 
 d3.json("includes/data/data.json", function(error, data) {
 
-  _datesRange = getDatesRange(data);
+  x.domain(getDatesRange(data));
+
+  var gapPercentage = 0.2;
+  var barWidth = Math.floor( (width / data.length) * (1-gapPercentage) );
 
   data.forEach(function(d) {
+      d.Date = new Date(d.year, d.month, 15);
       var children = getChildren(d);
       var cumulateHeight = 0;
       children.forEach(function(d) {
+          d.width = barWidth;
           d.y0 = cumulateHeight;
           d.height = y(d.opinionValue);
           cumulateHeight += d.height;
       });
   });
-
-  x.domain(data.map(function(d) { return d.month + "/" + d.year; }));
 
   svg.append("g")
       .attr("class", "x axis")
@@ -51,29 +53,25 @@ d3.json("includes/data/data.json", function(error, data) {
       .data(data)
     .enter().append("g")
       .attr("class", "month")
-      .attr("transform", function(d, i) { var date = d.month + "/" + d.year; return "translate(" + x(date) + ",0)"; });
+      .attr("transform", function(d) { return "translate(" + x(d.Date) + ",0)"; });
 
   month.selectAll("rect")
       .data(function(d) { return getChildren(d); })
     .enter().append("rect")
       .attr("y", function(d) { return d.y0; })
-      .attr("width", x.rangeBand())
+      .attr("width", function(d) {return d.width; })
       .attr("height", function(d) { return d.height; })
       .attr("percentage", function(d) { return d.opinionValue })
       .style("fill", function(d) { return colors[d.opinionIndex]; });
 
   d3.json("includes/data/events.json", function(error, events) {
 
-    xt.domain(_datesRange);
-
     svg.selectAll(".event")
         .data(events)
-      .enter().append("g")
-      .attr("class", "event")
-      .attr("transform", function(d) { d.Date = new Date(d.year, d.month, d.date); return "translate(" + xt(d.Date)-53 + "," + 100 + ")"; })
-      .append("circle")
-        .attr("r", 10)
-        .attr("cx", function(d) { return xt(d.Date); })
+      .enter().append("circle")
+        .attr("class", "event")
+        .attr("r", 5)
+        .attr("cx", function(d) { d.Date = new Date(d.year, d.month, d.date); console.log(d.Date); return x(d.Date); })
         .attr("cy", function(d) { return 100; })
         .on("mouseover", function(d) {
             div.transition()
