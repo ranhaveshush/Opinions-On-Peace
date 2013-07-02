22be860e47b4a14eb48791eb7ad944c74f998ef6
Ran Haveshush <ran.haveshush.shenkar@gmail.com>
2013-07-02 15:11:03 +0300

    Fixing the front vis

--- includes/js/scripts.js
+++ includes/js/scripts.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
 
-	$('#title h1').text(title);
-	$('#subtitle h3').text(subtitle);
-	$('#subtitle h2').text(questions[1]);
+	$('#title').text(title);
+	$('#subtitle').text(subtitle);
+	$('#question').text(questions[1]);
 
-	$('body').delegate('[id^="scroll"]', "click", function() {
+	$('#support').text(opinions[0][5]);
+	$('#oppose').text(opinions[0][1]);
+	$('#avoid').text(opinions[0][0]);
+
+	$('#authors').text(authors);
+	$('#reference span').text(referenceText);
+	$('#reference a').text('\"' + referenceLink + '\"');
+
+
+	$('body').delegate('[id^="scroll"] div', "click", function() {
 		var scrollDelta = 500;
-		var id = $(this).attr("id");
+		var id = $(this).parent().attr("id");
 		var svg = $("#data-vis svg");
 		var right = svg.css("right");
 		right = right.substring(0, right.length-2);
