d0b3d7d964d08b8e3090ca62c9119eda2359d509
Roni2012shemi <aharonshemi@gmail.com>
2013-07-03 18:02:51 +0300

    with font

---
+++ includes/js/scripts.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+	$('#title').text(_title);
+	$('#subtitle').text(_subtitle);
+	$('#question').text(_questions[1]);
+
+	$('#support').text(_opinions[0][5]);
+	$('#oppose').text(_opinions[0][1]);
+	$('#avoid').text(_opinions[0][0]);
+
+	$('#authors').text(_authors);
+	$('#reference span').text(_referenceText);
+	$('#reference a').text('\"' + _referenceLink + '\"');
+
+
+	$('body').delegate('[id^="scroll"] div', "click", function() {
+		var scrollDelta = 500;
+		var id = $(this).parent().attr("id");
+		var svg = $("#data-vis svg");
+		var right = svg.css("right");
+		right = right.substring(0, right.length-2);
+		right = parseInt(right, 10);
+		if (id === "scroll-old" && right >= -3685) {
+			right -= scrollDelta;
+		} else if (id === "scroll-new" && right < 0) {
+			right += scrollDelta;
+		}
+		svg.animate({right: right+"px"}, 1000);
+	});
+
+});
--- includes/js/visualize.js
+++ includes/js/visualize.js
@@ -1,24 +1,26 @@
 var _margin = {top: 50, right: 40, bottom: 20, left: 40},
-    width = 1200*4 - _margin.left - _margin.right,
-    height = 600 - _margin.top - _margin.bottom;
+    _width = 1200*4 - _margin.left - _margin.right,
+    _height = 400 - _margin.top - _margin.bottom;
 
-// Check if this vars have to be global
-var _yTimeline = height;
+var _yTimeline = -15;
+var _prevMonth;
+var _prevEventIndex = -1;
 
 var x = d3.time.scale()
-    .range([0, width]);
+    .range([0, _width]);
 
 var y = d3.scale.linear()
     .domain([0, 100])
-    .rangeRound([0, height]);
+    .rangeRound([0, _height]);
 
 var xAxis = d3.svg.axis()
     .scale(x)
-    .orient("bottom");
+    .ticks(d3.time.years, 1)
+    .orient("top");
 
-var svg = d3.select("#wrapper").append("svg")
-    .attr("width", width + _margin.left + _margin.right)
-    .attr("height", height + _margin.top + _margin.bottom)
+var svg = d3.select("#data-vis").append("svg")
+    .attr("width", _width + _margin.left + _margin.right)
+    .attr("height", _height + _margin.top + _margin.bottom)
   .append("g")
     .attr("transform", "translate(" + _margin.left + "," + _margin.top + ")");
 
@@ -26,31 +28,45 @@ d3.json("includes/data/data.json", function(error, data) {
 
   x.domain(getDatesRange(data));
 
-  var gapPercentage = 0.2;
-  var barWidth = Math.floor( (width / data.length) * (1-gapPercentage) );
+  var gapPercentage = 0.5;
+  var barWidth = Math.floor( (_width / data.length) * (1-gapPercentage) );
 
   data.forEach(function(d) {
-      d.Date = new Date(d.year, d.month, 15);
+      d.Date = new Date(d.year, d.month-1, 15);
       var children = getChildren(d);
       var cumulateHeight = 0;
-      children.forEach(function(d) {
-          d.width = barWidth;
-          d.y0 = cumulateHeight;
-          d.height = y(d.opinionValue);
-          cumulateHeight += d.height;
+      children.forEach(function(opinion) {
+          opinion.width = barWidth;
+          opinion.y0 = cumulateHeight;
+          opinion.height = y(opinion.opinionValue);
+          cumulateHeight += opinion.height;
       });
   });
 
   svg.append("g")
       .attr("class", "x axis")
-      .attr("transform", "translate(0," + _yTimeline + ")")
-      .call(xAxis);
+      .attr("transform", "translate(0, " + _yTimeline + ")")
+	  .style("stroke-dasharray", ("1, 3"))
+	  .attr("stroke", "#808080")
+	  .attr("fill", "#808080")
+	  .call(xAxis);
+	 svg.selectAll("g")	
+	.append("circle")
+        .attr("class", "circle")
+		.attr("cy", 0)
+        .attr("r", 4);
+		
+  var monthsContainer = svg.append("g")
+      .attr("class", "months");
 
-  var month = svg.selectAll(".month")
+  var month = monthsContainer.selectAll(".month")
       .data(data)
     .enter().append("g")
       .attr("class", "month")
-      .attr("transform", function(d) { return "translate(" + x(d.Date) + ",0)"; });
+      .attr("transform", function(d) { return "translate(" + x(d.Date) + ",0)"; })
+      .on("click", function(d) {
+          toogleMonth(this, d);
+      });
 
   month.selectAll("rect")
       .data(function(d) { return getChildren(d); })
@@ -59,29 +75,22 @@ d3.json("includes/data/data.json", function(error, data) {
       .attr("width", function(d) {return d.width; })
       .attr("height", function(d) { return d.height; })
       .attr("percentage", function(d) { return d.opinionValue })
-      .style("fill", function(d) { return colors[d.opinionIndex]; });
+      .style("fill", function(d) { return _colors[d.opinionIndex]; });
 
-  d3.json("includes/data/events.json", function(error, events) {
-  
-    svg.selectAll(".event")
-        .data(events)
+  d3.json("includes/data/events.json", function(error, data) {
+
+    var eventsContainer = svg.append("g")
+        .attr("class", "events");
+
+    eventsContainer.selectAll(".event")
+        .data(data)
       .enter().append("circle")
         .attr("class", "event")
-        .attr("r", 5)
-        .attr("cx", function(d) { d.Date = new Date(d.year, d.month, d.date); console.log(d.Date); return x(d.Date); })
-        .attr("cy", function(d) { return 100; })
-        .on("mouseover", function(d) {
-            div.transition()
-                .duration(200)
-                .style("opacity", .9);
-            div .html(formatTime(d.date) + "<br/>"  + d.close)
-                .style("left", (d3.event.pageX) + "px")
-                .style("top", (d3.event.pageY - 28) + "px");
-            })
-        .on("mouseout", function(d) {
-            div.transition()
-                .duration(500)
-                .style("opacity", 0);
+        .attr("r", 8)
+        .attr("cx", function(d) { d.Date = new Date(d.year, d.month-1, d.date); return x(d.Date); })
+        .attr("cy", function(d) { return _yTimeline; })
+        .on("click", function(d) {
+            toggleEvent(d);
         });
   });
 
@@ -89,13 +98,120 @@ d3.json("includes/data/data.json", function(error, data) {
 
 function getChildren(datum) {
     var nationalitiesIndex = datum.nationalitiesIndex;
-    return (nationalitiesIndex == 0) ? datum.children : datum.children[nationalitiesIndex].children;
+   return (nationalitiesIndex == 0) ? datum.children : datum.children[nationalitiesIndex].children;
 }
 
 function getDatesRange(data) {
     var firstEvent = data[0];
-    var minDate = new Date(firstEvent.year, firstEvent.month-1, (firstEvent.date) ? firstEvent.date : 1);
+    var minDate = new Date(firstEvent.year-1, firstEvent.month-1, (firstEvent.date) ? firstEvent.date : 1);
     var lastEvent = data[data.length-1];
-    var maxDate = new Date(lastEvent.year, lastEvent.month-1, (lastEvent.date) ? lastEvent.date : 1);
+    var maxDate = new Date(lastEvent.year+1, lastEvent.month, (lastEvent.date) ? lastEvent.date : 1);
+    console.log("nimDate=" + minDate);
+    console.log("maxDate=" + maxDate);
     return [minDate, maxDate];
+}
+
+function toogleMonth(element, month) {
+    // hide event-tooltip
+		$('#event-tooltip').hide();
+	
+	var dateContainer = $('#opinions-tooltip #date-container');
+    var opinionsContainer = $('#opinions-tooltip #opinions-container');
+	  
+    // clears previous tooltips content
+    dateContainer.empty();
+    opinionsContainer.empty();
+	
+    // gets the month data
+    var date = month.Date;
+   	// set correct question
+	var questioNum = (month.year < 2004)||((month.year == 2004)&&(month.month < 3)) ?0: 1;
+	
+	$('#question').text(_questions[questioNum]);
+	var opinionsIndex = month.opinionsIndex;
+	var opinions = (month.children.length == 6) ? month.children: month.children[month.children.length-1].children;
+	
+    //var question = questions[month.questionsIndex];
+
+    // renders date
+    var monthText = date.getMonth()+1;
+    var yearText = date.getFullYear();
+    var dateText = monthText + "-" + yearText;
+    dateContainer.text(dateText);
+
+    // calcs the white space between opinions
+    var length = opinions.length;
+    var whitespace = (1100 / length) - 150;
+    
+    // renders opinions
+    for (var i=0; i < length; ++i) {
+      var opinion = opinions[i];
+      var opinionIndex = opinion.opinionIndex;
+
+      var opinionText = _opinions[opinionsIndex][opinionIndex];
+      var opinionPercentage = opinion.opinionValue;
+      var opinionColor = _colors[opinionIndex];
+
+      var opinionElement = $('<section>')
+        .addClass('opinion')
+        .css('margin-left', whitespace + "px")
+        .append('<span class="opinion-text">' + opinionText + '</span>')
+        .append('<span class="opinion-percentage">' + opinionPercentage + '</span>')
+        .append('<div class="opinion-color" style="background-color:' + opinionColor + ';"></div>');
+
+      opinionsContainer.prepend(opinionElement);
+    }
+
+    // ugly but handling the toggle
+    if (_prevMonth && (month.year === _prevMonth.year && month.month === _prevMonth.month) ) {
+      $('#opinions-tooltip').toggle();
+      if (element.classList[1] === "selected") {
+        element.classList.remove("selected");
+        $('g[class="month"]').css("opacity", "1.0");
+      } else {
+        element.classList.add("selected");
+        $('g[class~="selected"]').css("opacity", "1.0");
+        $('g[class="month"]').each(function() {
+          $(this).css("opacity", "0.5");
+        });
+      }
+    } else {
+      $('#opinions-tooltip').show();
+      $('.selected').each(function() {
+        this.classList.remove("selected");
+      });
+      element.classList.add("selected");
+      $('g[class~="selected"]').css("opacity", "1.0");
+      $('g[class="month"]').each(function() {
+        $(this).css("opacity", "0.5");
+      });
+    }
+
+    _prevMonth = month;
+}
+
+function toggleEvent(event) {
+
+	// hide opinions-tooltip
+		$('#opinions-tooltip').hide();
+	var eventIndex = event.eventIndex;	
+    var eventName = _events[eventIndex];
+	
+	// gets the month data
+		var date = event.Date;
+	 // renders date
+    var monthText = date.getMonth()+1;
+    var yearText = date.getFullYear();
+    var dateText = monthText + "-" + yearText;	
+	var dataText = 	_eventText[eventIndex];
+    $('#event-tooltip #title h2').text(eventName+" "+dateText);
+    $('#event-tooltip #details p').text(dataText);
+    $('#event-tooltip img').attr("src", "includes/img/event_" + eventIndex + ".jpg");
+
+    if (eventIndex === _prevEventIndex) {
+      $('#event-tooltip').toggle();
+    } else {
+      $('#event-tooltip').show();
+    }
+    _prevEventIndex = eventIndex;
 }
